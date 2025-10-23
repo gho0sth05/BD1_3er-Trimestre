d97ee39b32808bc9dbcacc70ae34bf5337537470
lFonseca117 <laufonseca727@gmail.com>
2025-10-23 01:06:07 -0500

    Update formulariocrud.sql

--- formulariocrud_base_de_datos/formulariocrud.sql
+++ formulariocrud_base_de_datos/formulariocrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-10-2025 a las 09:08:46
+-- Tiempo de generación: 23-10-2025 a las 04:16:06
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -45,7 +45,24 @@ INSERT INTO `auditoria_reservas` (`id_auditoria`, `id_reserva`, `usuario`, `acci
 (2, 4, 'root@localhost', 'UPDATE', '2025-10-21 01:13:22', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
 (3, 1, 'root@localhost', 'UPDATE', '2025-10-21 01:54:11', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
 (4, 1, 'root@localhost', 'UPDATE', '2025-10-21 01:54:12', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
-(5, 2, 'root@localhost', 'UPDATE', '2025-10-21 01:55:34', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"');
+(5, 2, 'root@localhost', 'UPDATE', '2025-10-21 01:55:34', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(6, 7, 'root@localhost', 'UPDATE', '2025-10-21 03:59:49', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(7, 6, 'root@localhost', 'UPDATE', '2025-10-21 04:15:21', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
+(8, 5, 'root@localhost', 'UPDATE', '2025-10-21 04:48:34', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
+(9, 5, 'root@localhost', 'UPDATE', '2025-10-21 07:18:12', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
+(10, 8, 'root@localhost', 'UPDATE', '2025-10-21 07:18:15', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(11, 16, 'root@localhost', 'UPDATE', '2025-10-22 21:00:13', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(12, 13, 'root@localhost', 'UPDATE', '2025-10-22 21:02:11', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
+(13, 11, 'root@localhost', 'UPDATE', '2025-10-22 21:02:13', 'Reserva modificada: destino de \"Cartagena\" a \"Cartagena\", provincia \"Bolívar\"'),
+(14, 15, 'root@localhost', 'UPDATE', '2025-10-22 21:02:13', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(15, 14, 'root@localhost', 'UPDATE', '2025-10-22 21:02:15', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(16, 12, 'root@localhost', 'UPDATE', '2025-10-22 21:02:15', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(17, 10, 'root@localhost', 'UPDATE', '2025-10-22 21:02:17', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(18, 9, 'root@localhost', 'UPDATE', '2025-10-22 21:02:17', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(19, 19, 'root@localhost', 'UPDATE', '2025-10-22 21:04:07', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(20, 18, 'root@localhost', 'UPDATE', '2025-10-22 21:04:09', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(21, 17, 'root@localhost', 'UPDATE', '2025-10-22 21:04:13', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"'),
+(22, 20, 'root@localhost', 'UPDATE', '2025-10-22 21:04:38', 'Reserva modificada: destino de \"Medellín\" a \"Medellín\", provincia \"Antioquia\"');
 
 -- --------------------------------------------------------
 
@@ -82,7 +99,11 @@ INSERT INTO `compania` (`id_nombre`, `nombre`, `edad`, `fecha`, `vip`) VALUES
 (14, 'lolaas', 15, '2025-10-20', 'No'),
 (15, 'María Torres', 30, '2025-10-20', 'No'),
 (16, 'karmen', 16, '2025-10-20', 'No'),
-(17, 'lolaas', 778, '2025-10-20', 'No');
+(19, 'Camilo', 15, '2025-10-21', 'Sí'),
+(20, 'jesus', 54, '2025-10-21', 'Sí'),
+(21, 'laura', 18, '2025-10-22', 'Sí'),
+(22, 'Amanda', 46, '2025-10-22', 'Sí'),
+(23, 'Anderson', 20, '2025-10-22', 'No');
 
 -- --------------------------------------------------------
 
@@ -96,19 +117,37 @@ CREATE TABLE `reservas` (
   `destino` varchar(100) NOT NULL,
   `provincia` varchar(100) NOT NULL,
   `fecha_viaje` date NOT NULL,
-  `estado` enum('Pendiente','Confirmada','Cancelada') DEFAULT 'Pendiente'
+  `estado` enum('Pendiente','Confirmada','Cancelada') DEFAULT 'Pendiente',
+  `id_viaje` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `reservas`
 --
 
-INSERT INTO `reservas` (`id_reserva`, `id_cliente`, `destino`, `provincia`, `fecha_viaje`, `estado`) VALUES
-(1, 1, 'Cartagena', 'Bolívar', '2025-12-10', 'Cancelada'),
-(2, 2, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada'),
-(3, 11, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada'),
-(4, 11, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada'),
-(5, 8, 'Cartagena', 'Bolívar', '2025-12-10', 'Pendiente');
+INSERT INTO `reservas` (`id_reserva`, `id_cliente`, `destino`, `provincia`, `fecha_viaje`, `estado`, `id_viaje`) VALUES
+(1, 1, 'Cartagena', 'Bolívar', '2025-12-10', 'Cancelada', NULL),
+(2, 2, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(3, 11, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(4, 11, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(5, 8, 'Cartagena', 'Bolívar', '2025-12-10', 'Cancelada', NULL),
+(6, 16, 'Cartagena', 'Bolívar', '2025-12-10', 'Cancelada', NULL),
+(7, 16, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(8, 16, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(9, 16, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(10, 16, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(11, 16, 'Cartagena', 'Bolívar', '2025-12-10', 'Confirmada', NULL),
+(12, 20, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(13, 20, 'Cartagena', 'Bolívar', '2025-12-10', 'Confirmada', NULL),
+(14, 20, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(15, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Confirmada', NULL),
+(16, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(17, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(18, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(19, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(20, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Cancelada', NULL),
+(21, 22, 'Medellín', 'Antioquia', '2025-11-25', 'Pendiente', NULL),
+(22, 23, 'Medellín', 'Antioquia', '2025-11-25', 'Pendiente', NULL);
 
 --
 -- Disparadores `reservas`
@@ -171,21 +210,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `usuario`, `contraseña`, `rol`, `id_cliente`) VALUES
-(1, 'admin_viajes', 'admin123', 'Administrador', NULL),
-(2, 'empleado_viajes', 'empleado123', 'Empleado', NULL),
-(3, 'cliente_viajes', 'cliente123', 'Cliente', NULL),
-(4, 'laurag', '123456', 'Cliente', 6),
-(5, 'lolaaa71', '123456', 'Cliente', 7),
-(6, 'camilaaa', '123456', 'Cliente', 8),
-(7, 'lopex', '123456', 'Administrador', 9),
-(8, 'pepitas', '123456', 'Administrador', 10),
-(9, 'pepitasa', '123456', 'Cliente', 11),
-(10, 'loloo', '123456', 'Empleado', 12),
-(11, 'sofia24', '123456', 'Empleado', 13),
-(12, 'lolaaa70', '123456', 'Empleado', 14),
-(13, 'mariat', '123456', 'Cliente', 15),
-(14, 'karmesa', '123456', 'Cliente', 16),
-(15, 'camilaaas', '123456', 'Empleado', 17);
+(17, 'cami', 'qwer', 'Empleado', 19),
+(18, 'Jesus', '1234', 'Cliente', 20),
+(19, 'Laura', '123456', 'Administrador', 21),
+(20, 'Amanda', 'asd', 'Cliente', 22),
+(21, 'Anderson', '123456', 'Cliente', 23);
 
 -- --------------------------------------------------------
 
@@ -208,14 +237,14 @@ CREATE TABLE `viajes` (
 --
 
 INSERT INTO `viajes` (`id_viaje`, `destino`, `provincia`, `fecha_viaje`, `precio`, `imagen`, `estado`) VALUES
-(1, 'Cartagena', 'Bolívar', '2025-12-10', 1200000.00, 'https://images.unsplash.com/photo-1589391886645-d51941b9f5e4?w=800', 'Disponible'),
-(2, 'Medellín', 'Antioquia', '2025-11-25', 950000.00, 'https://images.unsplash.com/photo-1581167754154-8d2f77b36f03?w=800', 'Disponible'),
-(3, 'San Andrés', 'Archipiélago', '2025-12-22', 1450000.00, 'https://images.unsplash.com/photo-1605648916334-61a32a1c2b1a?w=800', 'Disponible'),
-(4, 'Bogotá', 'Cundinamarca', '2025-10-28', 800000.00, 'https://images.unsplash.com/photo-1614252862447-c1f0bbf0cdbd?w=800', 'Disponible'),
-(5, 'Pereira', 'Risaralda', '2025-11-30', 870000.00, 'https://images.unsplash.com/photo-1613747153669-3e3f0e279a52?w=800', 'Disponible'),
-(6, 'Santa Marta', 'Magdalena', '2025-12-05', 1100000.00, 'https://images.unsplash.com/photo-1602431130698-c1ec1ec9f1e1?w=800', 'Disponible'),
-(7, 'Cali', 'Valle del Cauca', '2025-12-15', 980000.00, 'https://images.unsplash.com/photo-1599946942471-b3c0b28ef8c2?w=800', 'Disponible'),
-(8, 'Barranquilla', 'Atlántico', '2025-11-18', 890000.00, 'https://images.unsplash.com/photo-1595922820061-6a8f7c776c79?w=800', 'Disponible');
+(14, 'Bogota', 'Cundinamarca', '2026-11-22', 852000.00, 'https://bogota.gov.co/sites/default/files/styles/1050px/public/2023-08/bogota_5.jpeg', 'Disponible'),
+(15, 'Barranquilla', 'Atlantico', '2025-11-17', 890000.00, 'https://probarranquilla.org/wp-content/uploads/2022/10/Banner-barranquilla5.jpg', 'Disponible'),
+(19, 'Medelli', 'Antioquia', '2025-11-17', 795000.00, 'https://caracol.com.co/resizer/v2/QEXZDVMAYZPXLOM23QEXI4YYJQ.jpg?auth=0acf5ae6fc83b83f453ef3cf7448a56f5035b70686f8de30489f73dfc43e9312&width=650&height=488&quality=70&smart=true', 'Disponible'),
+(22, 'Pereira', 'Risaralda', '2026-09-11', 870000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1NjhQidxmFxvoz-AeamfQT17GX5LT6LiBg&s', 'Disponible'),
+(23, 'San Andres', 'Archipiélago', '2026-01-22', 1100000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGt5J6xFL11Na6Y0GvjnMtwcXptpEzcPP5gA&s', 'Disponible'),
+(24, 'Cali', 'Valle del Cauca', '2026-03-13', 762000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0ss9VeIm7kyTc9YtHyt4Vg48m0p7L7RIfiw&s', 'Disponible'),
+(25, 'Cartagena ', 'Bolivar', '2025-10-25', 1120000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiY0BpiWpeGaLqEhUBfvcvgu5La4SdizJe3A&s', 'Disponible'),
+(26, 'Santa Marta', 'Magdalena', '2025-12-10', 932000.00, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeWtmu5PRqa3vAir1l2UkXmdIcmfkOSPm6FQ&s', 'Disponible');
 
 --
 -- Índices para tablas volcadas
@@ -239,7 +268,8 @@ ALTER TABLE `compania`
 --
 ALTER TABLE `reservas`
   ADD PRIMARY KEY (`id_reserva`),
-  ADD KEY `id_cliente` (`id_cliente`);
+  ADD KEY `id_cliente` (`id_cliente`),
+  ADD KEY `fk_reserva_viaje` (`id_viaje`);
 
 --
 -- Indices de la tabla `roles`
@@ -269,19 +299,19 @@ ALTER TABLE `viajes`
 -- AUTO_INCREMENT de la tabla `auditoria_reservas`
 --
 ALTER TABLE `auditoria_reservas`
-  MODIFY `id_auditoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_auditoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `compania`
 --
 ALTER TABLE `compania`
-  MODIFY `id_nombre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_nombre` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -293,13 +323,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `viajes`
 --
 ALTER TABLE `viajes`
-  MODIFY `id_viaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_viaje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- Restricciones para tablas volcadas
@@ -315,6 +345,7 @@ ALTER TABLE `auditoria_reservas`
 -- Filtros para la tabla `reservas`
 --
 ALTER TABLE `reservas`
+  ADD CONSTRAINT `fk_reserva_viaje` FOREIGN KEY (`id_viaje`) REFERENCES `viajes` (`id_viaje`),
   ADD CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `compania` (`id_nombre`);
 
 --
